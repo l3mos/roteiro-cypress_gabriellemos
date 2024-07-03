0bdefc82ae07be9f0b6773bb01e47cff2eac95b2
Gabriel Lemos <92003618+l3mos@users.noreply.github.com>
2024-07-03 15:00:12 -0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 describe('template spec', () => {
   it('Verifica se app está abrindo', () => {
     cy.visit('http://127.0.0.1:7001/')
-  })
+  });
 
   it('Insere uma tarefa', () => {
     cy.visit('http://127.0.0.1:7001'); 
@@ -55,5 +55,69 @@ describe('template spec', () => {
     cy.contains('All').click();
     cy.get('.todo-list li')
       .should('have.length', 2);
+  });
+
+  // Novo teste 1: Editar uma tarefa
+  it('Edita uma tarefa existente', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('TP2 de Engenharia de Software{enter}');
+
+    cy.get('.todo-list li')
+      .dblclick()
+      .find('.edit')
+      .clear()
+      .type('TP2 de ES - Editado{enter}');
+
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'TP2 de ES - Editado');
+  });
+
+  // Novo teste 2: Marcar todas as tarefas como completas e depois como incompletas
+  it('Marca todas as tarefas como completas e depois como incompletas', () => {
+    cy.visit('http://127.0.0.1:7001'); 
+
+    cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    cy.get('.todo-list li .toggle')
+      .each(($el) => {
+        cy.wrap($el).click();
+      });
+
+    cy.get('.todo-list li')
+      .each(($el) => {
+        cy.wrap($el).should('have.class', 'completed');
+      });
+
+    cy.get('.todo-list li .toggle')
+      .each(($el) => {
+        cy.wrap($el).click();
+      });
+
+    cy.get('.todo-list li')
+      .each(($el) => {
+        cy.wrap($el).should('not.have.class', 'completed');
+      });
+  });
+
+  // Novo teste 3: Adicionar múltiplas tarefas e verificar sua presença
+  it('Adiciona múltiplas tarefas e verifica se todas estão presentes', () => {
+    cy.visit('http://127.0.0.1:7001'); 
+
+    cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}')
+      .type('Estudar para provas{enter}');
+
+    cy.get('.todo-list li')
+      .should('have.length', 3)
+      .eq(0).should('have.text', 'TP2 de ES')
+      .next().should('have.text', 'Prova de ES')
+      .next().should('have.text', 'Estudar para provas');
   });
 });
